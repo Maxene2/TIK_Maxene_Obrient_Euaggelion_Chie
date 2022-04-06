908e20846443769a88985208475894a07464d6e4
Max <103086015+Maxene2@users.noreply.github.com>
2022-04-06 11:26:50 +0800

    Create 20220406_Input Output.cpp

---
+++ 20220406_Input Output.cpp
@@ -0,0 +1,9 @@
+//header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+    string nama;
+    cout<<"Masukan nama: "; cin>>nama;
+   cout<<"Nama Anda: "<<nama;
+   
+}//end of function

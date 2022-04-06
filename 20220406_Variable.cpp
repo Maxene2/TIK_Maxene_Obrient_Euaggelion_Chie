8a1f4c0cbd9c989f33d80c91d7ba6f1939fe7d90
Max <103086015+Maxene2@users.noreply.github.com>
2022-04-06 11:08:47 +0800

    Create 20220406_Variable.cpp

---
+++ 20220406_Variable.cpp
@@ -0,0 +1,14 @@
+//header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+     float angka;
+     float buku;
+     string nama; //deklarasi variabel
+     nama="Maxene Obrient Euaggelion Chie"; //masukkan value ke dalam variable
+     angka = 10.21;
+     buku = 3.14;
+     cout<<nama<<endl;//menampilkan variable
+     cout<<angka + buku; 
+     
+}//end of function

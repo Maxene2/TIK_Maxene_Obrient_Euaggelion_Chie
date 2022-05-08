7507e754c55281d4a73c76279279f9a7ece0be62
Max <103086015+Maxene2@users.noreply.github.com>
2022-05-08 22:35:31 +0800

    Create 20220508_While pt1.cpp

---
+++ 20220508_While pt1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+ using namespace std;
+ int main () {
+     int number1 = 0;
+     while (number1 ++ <10) { //perulangan turun
+        cout<<"1"<<""<<number1<<"="<<number11<<endl;
+     }
+       cout<<endl<<endl;
+
+int number2 = 0;
+     while (number2 ++ <10) { //perulangan turun
+        cout<<"2"<<""<<number2<<"="<<number22<<endl;
+     }
+       cout<<endl<<endl;
+
+int number3 = 0;
+     while (number3 ++ <10) { //perulangan turun
+        cout<<"3"<<""<<number3<<"="<<number33<<endl;
+     }
+       cout<<endl<<endl;
+
+int number4 = 0;
+     while (number4 ++ <10) { //perulangan turun
+        cout<<"4"<<""<<number4<<"="<<number44<<endl;
+     }
+       cout<<endl<<endl;
+
+int number5 = 0;
+     while (number5 ++ <10) { //perulangan turun
+        cout<<"5"<<""<<number5<<"="<<number55<<endl;
+     }
+       cout<<endl<<endl;
+
+int number6 = 0;
+     while (number6 ++ <10) { //perulangan turun
+        cout<<"6"<<""<<number6<<"="<<number66<<endl;
+     }
+       cout<<endl<<endl;
+
+int number7 = 0;
+     while (number7 ++ <10) { //perulangan turun
+        cout<<"7"<<""<<number7<<"="<<number77<<endl;
+     }
+       cout<<endl<<endl;
+
+int number8 = 0;
+     while (number8 ++ <10) { //perulangan turun
+        cout<<"8"<<""<<number8<<"="<<number88<<endl;
+     }
+       cout<<endl<<endl;
+
+int number9 = 0;
+     while (number9 ++ <10) { //perulangan turun
+        cout<<"9"<<""<<number9<<"="<<number99<<endl;
+     }
+       cout<<endl<<endl;
+
+int number10 = 0;
+     while (number10 ++ <10) { //perulangan turun
+        cout<<"10"<<""<<number10<<"="<<number1010<<endl;
+     }
+       cout<<endl<<endl;
+ }

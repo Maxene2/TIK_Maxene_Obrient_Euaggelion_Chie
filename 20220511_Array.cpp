3fc5840aa758ff5f55d3124676a6960514463e82
Max <103086015+Maxene2@users.noreply.github.com>
2022-05-11 10:58:15 +0800

    Create 20220511_Array.cpp

---
+++ 20220511_Array.cpp
@@ -0,0 +1,41 @@
+//array 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nama[] = {
+    "David", 
+    "Kabila", 
+    "Bryan", 
+    "Sidney",
+    "Gracia",
+    "Keysie",
+    "Yoel"
+    };   
+        
+    for(int i = 0; i<7 ; i ++){
+    cout<<nama[i]<<endl;
+    
+}
+}
+
+
+//array data integer
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int angka[] = {
+        5,7,8,9,2
+    };
+    for(int j = 0; j<5; j++){
+}
+
+for (int i = 0; i<5 ; i ++){
+    cout<<"angke ke - "<<i<<" = "<<angka[i]*angka[i]<<endl;
+}
+} 

fd6dee33f481d634d014908f5c830b4719e58800
Max <103086015+Maxene2@users.noreply.github.com>
2022-05-11 11:29:30 +0800

    Create 20220511_ Array(unfinisehd).cpp

---
+++ 20220511_ Array(unfinisehd).cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int A[2] [2]= {
+    }
+    { 1},
+        {4 8},
+     };
+ for(int A = 0; i<2; i++){
+ 
+ }
+
+    int Angka B[2] [2]= {
+        {5 2},
+        {8 3},
+    for(int Angka B = 0; j<2; j++){
+    }
+};
+    cout<<int A[i][j] + B[i][j]
+}
